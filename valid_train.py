9821704c2b5306cb218f9bf01de93aa1656bbb50
Marco <marco.cartago@outlook.it>
2025-09-06 15:25:07 +0200

    Finished building an outline of the main validation loop. Still has to be expanded and checked

--- valid_train.py
+++ valid_train.py
@@ -4,13 +4,15 @@ import pickle
 import tqdm
 
 from dotenv import load_dotenv
-from sklearn.model_selection import KFold
 
 import torch
+from torch.utils.data import DataLoader
 import pandas as pd
 import h5py
+from sklearn.model_selection import KFold
 
 import networks as nets
+import data_preprocess_mol as preprocess_mol
 
 MODEL_CLASSES = [
     cls for name, cls in vars(nets).items()
@@ -24,30 +26,49 @@ UPSCALE_DIM = 128
 MODELS = {
 
     # To test a model that just does aggregation
-    "JustAggrConvGCNN": nets.JustAggrConvGCNN(
-        in_dim=IN_EMB_DIM, out_dim=OUT_EMB_DIM, upscale_dim=UPSCALE_DIM),
+    "JustAggrConvGCNN": (nets.JustAggrConvGCNN,
+                         (IN_EMB_DIM, OUT_EMB_DIM, UPSCALE_DIM)),
 
     # A single convolution with average aggregation
-    "OneConvGCNN": nets.OneConvGCNN(
-        in_dim=IN_EMB_DIM, out_dim=OUT_EMB_DIM, upscale_dim=UPSCALE_DIM),
+    "OneConvGCNN": (nets.OneConvGCNN,
+                    (IN_EMB_DIM, OUT_EMB_DIM, UPSCALE_DIM)),
 
     # Two convolutions with average aggregation
-    "TwoConvGCNN": nets.TwoConvGCNN(
-        in_dim=IN_EMB_DIM, out_dim=OUT_EMB_DIM, upscale_dim=UPSCALE_DIM),
+    "TwoConvGCNN": (nets.TwoConvGCNN,
+                    (IN_EMB_DIM, OUT_EMB_DIM, UPSCALE_DIM)),
 
     # Graph attention network with one head
-    "OneHeadGAT": nets.OneHeadGAT(
-        in_dim=IN_EMB_DIM, out_dim=OUT_EMB_DIM, upscale_dim=UPSCALE_DIM),
+    "OneLayerOneHeadGAT": (nets.OneLayerOneHeadGAT,
+                           (IN_EMB_DIM, OUT_EMB_DIM, UPSCALE_DIM)),
 
     # Graph attention network with two heads
-    "MultiHeadGAT-2": nets.MultiHeadGAT(
-        in_dim=IN_EMB_DIM, out_dim=OUT_EMB_DIM, upscale_dim=UPSCALE_DIM,
-        n_head=2),
+    "MultiHeadGAT-2": (nets.MultiHeadGAT,
+                       (IN_EMB_DIM, OUT_EMB_DIM, UPSCALE_DIM, 2)),
 
     # Graph attention with four heads
-    "MultiHeadGAT-4": nets.MultiHeadGAT(
-        in_dim=IN_EMB_DIM, out_dim=OUT_EMB_DIM, upscale_dim=UPSCALE_DIM,
-        n_head=4),
+    "MultiHeadGAT-4": (nets.MultiHeadGAT,
+                       (IN_EMB_DIM, OUT_EMB_DIM, UPSCALE_DIM, 4)),
+}
+
+MODELS_NCONV_HEADS = {
+
+    # To test a model that just does aggregation
+    "JustAggrConvGCNN": (0, 0),
+
+    # A single convolution with average aggregation
+    "OneConvGCNN": (1, 0),
+
+    # Two convolutions with average aggregation
+    "TwoConvGCNN": (2, 0),
+
+    # Graph attention network with one head
+    "OneLayerOneHeadGAT": (21, 1),
+
+    # Graph attention network with two heads
+    "MultiHeadGAT-2": (2, 2),
+
+    # Graph attention with four heads
+    "MultiHeadGAT-4": (2, 4),
 }
 
 
@@ -77,6 +98,7 @@ def train_model(model, num_epochs, trainloader, criterion, optimizer,
         epoch_valid_loss = []
 
         start = time.time()
+
         # Training
         model.train()
         for graphs, labels in trainloader:
@@ -135,10 +157,11 @@ def train_model(model, num_epochs, trainloader, criterion, optimizer,
 
 class RunResult():
 
-    def __init__(self, batch_size, learning_rate, net_type, n_head, e_size, n_conv):
+    def __init__(self, fold_num, batch_size, learning_rate, net_type, n_head, e_size, n_conv):
 
         self.batch_size = batch_size
         self.learning_rate = learning_rate
+        self.fold_num = fold_num
 
         self.n_type = net_type
         self.n_head = n_head
@@ -149,3 +172,109 @@ class RunResult():
         self.v_losses = []
         self.t_epoch_avg_losses = []
         self.v_epoch_avg_losses = []
+
+
+load_dotenv()
+
+# Set arguments
+mol_data_path = "./mol-data/mol_sample_metadata.csv"
+metadata = pd.read_csv(mol_data_path)
+
+# Path to the h5py file containing all the preprocessed embeddings, labels
+# and adjacency matrix
+treated_molecules_path = os.getenv("TREATED_MOLECULES_PATH")
+if treated_molecules_path is None:
+    raise ValueError("Invalid .h5 data path.")
+
+h5f = h5py.File(treated_molecules_path, "r")
+
+train_set: pd.DataFrame = metadata[metadata["set"] == "train"]
+
+NUM_EPOCHS = 200
+BATCH_SIZE = 32
+START_LEARNING_RATE = 0.01
+STEP_SIZE = 20
+
+
+def kfold_validate_models(model_class_name, num_folds=5):
+
+    run_result_list = []
+    kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
+
+    for fold, (train_idx, val_idx) in enumerate(kf.split(train_set)):
+        # Split the data based on train/test indices
+        train_subset = train_set.iloc[train_idx].reset_index(drop=True)
+        valid_subset = train_set.iloc[val_idx].reset_index(drop=True)
+
+        # Load and preprocess the data
+        train_subset_dataset = preprocess_mol.H5PyMolDataset(
+            h5f, train_subset, "idx", train=True)
+        train_dataloader = DataLoader(
+            train_subset_dataset, batch_size=BATCH_SIZE,
+            shuffle=False, collate_fn=preprocess_mol.graph_collate_fn_h5)
+
+        valid_subset_dataset = preprocess_mol.H5PyMolDataset(
+            h5f, valid_subset, "idx", train=True)
+        valid_dataloader = DataLoader(
+            valid_subset_dataset, batch_size=BATCH_SIZE,
+            shuffle=False, collate_fn=preprocess_mol.graph_collate_fn_h5)
+
+        # Initialize model, optimizer, scheduler
+        model_class, args = MODELS[model_class_name]
+        model_istance = model_class(*args)
+
+        # Define criterion, optimizer and scheduler
+        criterion = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(
+            model_istance.parameters(),
+            lr=START_LEARNING_RATE
+        )
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer,
+            step_size=STEP_SIZE,
+            gamma=0.80,
+        )
+
+        # Train the model
+        train_losses, valid_losses, avg_train_losses, avg_valid_losses = \
+            train_model(model_istance,
+                        num_epochs=NUM_EPOCHS,
+                        trainloader=train_dataloader,
+                        validloader=valid_dataloader,
+                        criterion=criterion,
+                        optimizer=optimizer,
+                        scheduler=scheduler,
+                        verbose=True,
+                        dump=True)
+
+        # Store and process the results
+        n_conv, n_heads = MODELS_NCONV_HEADS[model_class_name]
+
+        run_result = RunResult(fold_num=fold,
+                               batch_size=BATCH_SIZE,
+                               learning_rate=START_LEARNING_RATE,
+                               net_type=model_class_name,
+                               n_head=n_conv,
+                               e_size=UPSCALE_DIM,
+                               n_conv=n_heads)
+
+        run_result.t_losses = train_losses
+        run_result.v_losses = valid_losses
+        run_result.t_epoch_avg_losses = avg_train_losses
+        run_result.v_epoch_avg_losses = avg_valid_losses
+        run_result_list.append(run_result)
+
+    return run_result_list
+
+
+def main():
+
+    obj = [kfold_validate_models(cl_name.__name__)
+           for cl_name in MODEL_CLASSES]
+
+    with open("./model-outputs/out_valid.pkl", "wb") as f:
+        pickle.dump(obj, f)
+
+
+if __name__ == "__main__":
+    main()
